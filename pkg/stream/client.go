7e96ff0712221057447a2422ee8d8fcbee1b03f4
Tomato <1654828489@qq.com>
2026-01-12 22:36:22 +0800

    fix stream client

--- pkg/stream/client.go
+++ pkg/stream/client.go
@@ -236,7 +236,7 @@ func (c *StreamClient) handleControlMessage(data []byte) {
 			log.Errorf("stream: control response error: %v\n", resp.Error)
 		}
 		if resp.Id != 0 {
-			log.Errorf("stream: sub response received for id %d\n", resp.Id)
+			log.Println("stream: sub response received for id %d\n", resp.Id)
 		}
 	}
 }
